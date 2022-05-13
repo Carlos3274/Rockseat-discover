2362e942a5f84eb86bbddd5f8bd96f03b9030d7f
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-05-13 12:45:38 -0300

    update

--- NLW/main.js
+++ NLW/main.js
@@ -5,5 +5,12 @@ function onScroll(){
     } else{
         navigation.classList.remove('scroll')
     }
-    
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
 }
