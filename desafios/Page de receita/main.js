7c84a8fe018ea2b120f3a1460dc8abe45b2caf2a
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-05-24 13:39:46 -0300

    update

---
+++ desafios/Page de receita/main.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: '700',
+/* Separar as strings com acento agudo `` permite separa-las melhor(TEMPLATE LITERALS ) */
+}).reveal(`
+  h1, 
+  figure, 
+  h2,
+  ul,
+  ol,
+  footer`);
